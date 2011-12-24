f7c1067eaf804b847f1aff0fd90c591b06892571
math314 <314same@gmail.com>
2011-12-24 20:54:11 +0900

    なんか描画できるようになった

--- Form1.cs
+++ Form1.cs
@@ -11,9 +11,37 @@ namespace git_test
 {
     public partial class Form1 : Form
     {
+
+        private mineController ctl = new mineController();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// クリックされた
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 描画時
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            ctl.OnPaint(pictureBox1.Size, e.Graphics);
+        }
+
+        private void pictureBox1_Resize(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
         }
     }
 }
---
+++ mineController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace git_test
+{
+    public class mineController
+    {
+
+        /// <summary> 縦方向のブロック数 </summary>
+        public int VBlock;
+        /// <summary> 横方向のブロック数 </summary>
+        public int HBlock;
+
+        public mineController()
+        {
+            VBlock = 10;
+            HBlock = 15;
+        }
+
+        /// <summary>
+        /// 描画時に呼び出されます
+        /// </summary>
+        /// <param name="size"></param>
+        /// <param name="g"></param>
+        public void OnPaint(SizeF size, Graphics g)
+        {
+            g.Clear(Color.White);
+
+            for (int v = 0; v < VBlock; v++)
+            {
+                for (int h = 0; h < HBlock; h++)
+                {
+                    OnCellDraw(new RectangleF(size.Width * h / HBlock, size.Height * v / VBlock,
+                        size.Width / HBlock, size.Height / VBlock),
+                        g);
+                }
+            }
+        }
+
+        /// <summary>
+        /// セルの描画
+        /// </summary>
+        /// <param name="loc"></param>
+        /// <param name="size"></param>
+        /// <param name="g"></param>
+        private void OnCellDraw(RectangleF rect , Graphics g)
+        {
+            g.DrawRectangle(Pens.Aquamarine, Rectangle.Ceiling(rect));
+            //g.FillRectangle(Brushes.Aqua, rect);
+        }
+    }
+}
