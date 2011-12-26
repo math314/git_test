017711dfcf4574ef42b2d22bb07afc6e7245f7ac
math314 <314same@gmail.com>
2011-12-27 00:08:26 +0900

    コンソールでやる感じになったりしてる

--- Program.cs
+++ Program.cs
@@ -1,21 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Forms;
 
 namespace git_test
 {
     static class Program
     {
-        /// <summary>
-        /// アプリケーションのメイン エントリ ポイントです。
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static int Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            MineView view = new MineView();
+
+            view.Draw();
+
+            return 0;
         }
     }
 }
